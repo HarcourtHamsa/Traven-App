f54e59e52e77f98232f3f18a12c90b21aa6c970b
Harcourt Hamsa <hamsaharcourt@gmail.com>
2020-02-25 23:58:01 -0200

    backend working, mail sending function is not working yet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
+import Appointments from "./components/Appointments";
 
 export default function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/appointments" component={Appointments} />
       </Switch>
     </Router>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react'
-import Logo from './Logo';
+import React, { Component } from "react";
 
 export default class Footer extends Component {
-    state = {};
+  state = {};
 
-
-    render() {
-        return (
-            <div className="bg-light p-3 fixed-bottom">
-                <Logo />
-                <p><b>Email</b>: hamsaharcourt@gmail.com</p>
-                <p><b>Phone</b>: +2348075339205</p>
-                <p>Copyright &copy; of Harcourt Hamsa 2020</p>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="bg-primary text-white p-3 mt-3">
+        <div>
+          <i className="fa fa-github h4 m-2"></i>
+          <i className="fa fa-twitter h4 m-2"></i>
+          <i className="fa fa-facebook h4 m-2"></i>
+        </div>
+        <p>Email: hamsaharcourt@gmail.com</p>
+        <p>Phone: +2348075339205</p>
+        <p>Copyright &copy; of Harcourt Hamsa 2020</p>
+      </div>
+    );
+  }
 }
